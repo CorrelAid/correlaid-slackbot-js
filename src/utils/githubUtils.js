c389233db68787fe313456ad1316fcbcb0592ffe
Frie <fr1e@pm.me>
2020-01-25 18:22:21 +0100

    different env files dep on stage, fix GH Api call

--- src/utils/githubUtils.js
+++ src/utils/githubUtils.js
@@ -1,54 +1,53 @@
 require('dotenv').config()
 const axios = require('axios')
-const Octokit = require("@octokit/rest");
+const Octokit = require('@octokit/rest')
 const GITHUB_PAT = process.env.GITHUB_PAT
 const GITHUB_OWNER = process.env.GITHUB_OWNER
 const GITHUB_REPO = process.env.GITHUB_REPO
 const GITHUB_FILE = process.env.GITHUB_FILE
 
-
 const octokit = new Octokit({
-  auth: GITHUB_PAT
-});
+    auth: GITHUB_PAT,
+})
 
-const getFile = async file => {
-
+const getFile = async () => {
     return await octokit.repos.getContents({
         owner: GITHUB_OWNER,
         repo: GITHUB_REPO,
-        path: GITHUB_FILE
-      })
+        path: GITHUB_FILE,
+    })
 }
 const addUrlToReadme = async (url, title) => {
-    readmeFile = await getFile(GITHUB_FILE)
+    readmeFile = await getFile()
     // add url
-    linkBase = createLinkTextBase64(url, title)
-    newContent = readmeFile.data.content + linkBase
+    newContent = createBase64Content(readmeFile.data.content, url, title)
     sha = readmeFile.data.sha
     commitMessage = `added link for ${url}`
-    await updateFile(GITHUB_FILE, sha, newContent, commitMessage)
+    await updateFile(sha, newContent, commitMessage)
 }
 
-const createLinkTextBase64 = (url, title) => {
-    linkStr = `- [${title}](${url})\n`
-    const linkBase = Buffer.from(linkStr).toString('base64')
-    return linkBase
+const createBase64Content = (oldContent, url, title) => {
+    // decode old content
+    const toAdd = `- [${title}](${url})`
+    const oldContentDec = Buffer.from(oldContent, "base64").toString('utf-8');
+    let newContent = Buffer.from(oldContentDec + "\n" + toAdd).toString('base64');
+
+    return newContent
 }
 
-const updateFile = async (file, sha, newContent, commitMessage) => {
-
+const updateFile = async (sha, newContent, commitMessage) => {
     await octokit.repos.createOrUpdateFile({
         owner: GITHUB_OWNER,
         repo: GITHUB_REPO,
         path: GITHUB_FILE,
         message: commitMessage,
         content: newContent,
-        sha: sha, 
+        sha: sha,
     })
 }
 
 module.exports = {
     getFile,
     addUrlToReadme,
-    createLinkTextBase64,
+    createBase64Content,
 }
